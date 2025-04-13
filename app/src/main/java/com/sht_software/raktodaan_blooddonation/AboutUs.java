d2dce43798bd179ee474f9fabde530a262be7fdf
Freelancer RJ Tanvir <tanverahmed.playstore2023@gmail.com>
2025-04-13 13:40:18 +0600

    AboutUs.java

--- app/src/main/java/com/sht_software/raktodaan_blooddonation/AboutUs.java
+++ app/src/main/java/com/sht_software/raktodaan_blooddonation/AboutUs.java
@@ -1,6 +1,11 @@
 package com.sht_software.raktodaan_blooddonation;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,92 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.sht_software.raktodaan_blooddonation.databinding.ActivityAboutUsBinding;
+
 public class AboutUs extends AppCompatActivity {
+
+    private ActivityAboutUsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about_us);
+        binding = ActivityAboutUsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // Control Stop Rotation
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        aboutMe();
+
+        binding.call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:01682822568"));
+                startActivity(intent);
+            }
+        });
+
+        binding.facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://facebook.com/rjtanvir321";
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+
+        binding.back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+
     }
-}
+    private void aboutMe () {
+
+        String url = "https://shtacademy.com/Software/aboutMe.php";
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("ressss", response.toString());
+                        binding.aboutMe.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+} // last brackets
--- app/src/main/java/com/sht_software/raktodaan_blooddonation/Fragment/UpdateInfo.java
+++ app/src/main/java/com/sht_software/raktodaan_blooddonation/Fragment/UpdateInfo.java
@@ -1,5 +1,6 @@
 package com.sht_software.raktodaan_blooddonation.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sht_software.raktodaan_blooddonation.AboutUs;
 import com.sht_software.raktodaan_blooddonation.R;
 import com.sht_software.raktodaan_blooddonation.databinding.FragmentUpdateInfoBinding;
 
@@ -21,8 +23,12 @@ public class UpdateInfo extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentUpdateInfoBinding.inflate(inflater, container, false);
 
-
-
+        binding.aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), AboutUs.class));
+            }
+        });
 
 
         return binding.getRoot();
