a3d8c95623adaa2fe89cbb4e67cd612df7da210a
Freelancer RJ Tanvir <tanverahmed.playstore2023@gmail.com>
2025-04-13 18:32:30 +0600

    Update Info

--- app/src/main/java/com/sht_software/raktodaan_blooddonation/UpdateActivity.java
+++ app/src/main/java/com/sht_software/raktodaan_blooddonation/UpdateActivity.java
@@ -5,6 +5,7 @@ import static android.view.View.VISIBLE;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.textfield.TextInputEditText;
 import com.sht_software.raktodaan_blooddonation.databinding.ActivityUpdateBinding;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class UpdateActivity extends AppCompatActivity {
 
@@ -31,6 +38,19 @@ public class UpdateActivity extends AppCompatActivity {
             return insets;
         });
 
+        binding.buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UpdateActivity.this, "Update date", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.buttonAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(UpdateActivity.this, "Update Address", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         // Set initial button colors
         binding.btnDonationDate.setBackgroundColor(ContextCompat.getColor(this, R.color.primary_red));
@@ -62,8 +82,33 @@ public class UpdateActivity extends AppCompatActivity {
             }
         });
 
-        // You can also add your other initialization code here
-        // For example, setting up the date picker for lastDonateEditText
-        // or initializing the spinners
+        binding.lastDonateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDatePicker();
+            }
+        });
+
+        binding.lastDonateInputLayout.setEndIconOnClickListener(view -> {
+            showDatePicker();
+        });
+
+    } // On Create bundle
+
+    private void showDatePicker() {
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date")
+                .build();
+
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+            // Convert the selected timestamp to a readable date
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+            String formattedDate = sdf.format(new Date(selection));
+
+            // Set the date in the EditText
+            ((TextInputEditText) UpdateActivity.this.findViewById(R.id.lastDonateEditText)).setText(formattedDate);
+        });
+
+        datePicker.show(UpdateActivity.this.getSupportFragmentManager(), "DATE_PICKER");
     }
 }
