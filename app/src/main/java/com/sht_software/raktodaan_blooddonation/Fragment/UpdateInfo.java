948d165fb3a11973bf89fc5363980966f296466b
Freelancer RJ Tanvir <tanverahmed.playstore2023@gmail.com>
2025-04-13 13:57:59 +0600

    Lots of update

--- app/src/main/java/com/sht_software/raktodaan_blooddonation/Fragment/UpdateInfo.java
+++ app/src/main/java/com/sht_software/raktodaan_blooddonation/Fragment/UpdateInfo.java
@@ -1,6 +1,8 @@
 package com.sht_software.raktodaan_blooddonation.Fragment;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 
 import com.sht_software.raktodaan_blooddonation.AboutUs;
 import com.sht_software.raktodaan_blooddonation.R;
+import com.sht_software.raktodaan_blooddonation.WebView;
 import com.sht_software.raktodaan_blooddonation.databinding.FragmentUpdateInfoBinding;
 
 public class UpdateInfo extends Fragment {
@@ -27,6 +30,40 @@ public class UpdateInfo extends Fragment {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getActivity(), AboutUs.class));
+            }
+        });
+
+        binding.privacyPolicy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                WebView.url = "https://sites.google.com/view/sht-kabinnama";
+                startActivity(new Intent(getActivity(), WebView.class));
+            }
+        });
+
+        binding.ourMoreApps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String developerName = "https://play.google.com/store/apps/dev?id=7635698146769202465";
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(""+developerName)));
+                } catch (ActivityNotFoundException e) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(""+developerName)));
+                }
+            }
+        });
+
+        binding.rateUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        binding.shareApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
---
+++ app/src/main/java/com/sht_software/raktodaan_blooddonation/WebView.java
@@ -0,0 +1,104 @@
+package com.sht_software.raktodaan_blooddonation;
+
+import android.app.DownloadManager;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.Window;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
+import android.webkit.WebChromeClient;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class WebView extends AppCompatActivity {
+
+    android.webkit.WebView webview;
+    LottieAnimationView loading;
+    public static String url = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_web_view);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webview = findViewById(R.id.webview);
+        loading = findViewById(R.id.loading);
+
+        getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+
+        webview = (android.webkit.WebView) findViewById(R.id.webview);
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(android.webkit.WebView view, int progress)
+            {
+                //Make the bar disappear after URL is loaded, and changes string to Loading...
+                //setTitle("Loading...");
+                setProgress(progress * 100); //Make the bar disappear after URL is loaded
+
+                // Return the app name after finish loading
+                if(progress == 100){
+                    loading.setVisibility(View.INVISIBLE);
+                }else {
+                    loading.setVisibility(View.VISIBLE);
+                }
+
+            }
+        });
+        webview.getSettings().setBuiltInZoomControls(true);
+        webview.setWebViewClient(new HelloWebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl(url);
+
+        // Control Stop Rotation
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
+        webview.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimetype);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookic", cookies);
+
+                request.addRequestHeader("userAgent", userAgent);
+                request.setDescription("Download file");
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+                DownloadManager DM = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                DM .enqueue(request);
+                Toast.makeText(WebView.this, "Downloading file", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }// OnCreate method Close Here =======================
+
+    private class HelloWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(android.webkit.WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+} // Public Class Close Here===============
